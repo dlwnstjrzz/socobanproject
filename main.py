e4af48f2f3c19838a11df4d3434dd5fd4f5a6d60
Noahcodee <dlwnstjr37@gmail.com>
2021-12-08 01:11:58 +0900

    단계1에서 원하는 형식으로 출력되게 최종 수정

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 def makeMapToArr ():
     maps = []
     mapFile = open("map.txt", "r")
-
     lines = mapFile.readlines()
     rowSize = 0
+    hall = 0
+    ball = 0
     stage = []
+    position = []
     for line in lines:
         if 'Stage' in line or '=' in line:
             if len(stage) > 0:
                 for i in range(len(stage)):
                     while len(stage[i]) < rowSize:
-                        stage[i].append(' ')
-
+                        stage[i].append(' ') #stage1이 끝난 시점
+                print(f"가로크기: {rowSize}\n세로크기: {len(stage)}\n구멍의 수:{hall}\n공의 수 :{ball}\n플레이어 위치 ({position[0] + 1}, {position[1]}) ")
                 maps.append(stage)
                 stage = []
-                rowSize = 0
+                rowSize - 0
+                hall = 0
+                ball = 0
+                position = []
+            if '=' in line:
+                continue
+
+            print(''.join(line))
             continue
 
-
+        print(''.join(line))
         rowSize = max(len(line.rstrip()), rowSize)
         row = []
         for cell in line:
@@ -25,15 +34,22 @@ def makeMapToArr ():
                 row.append('0')
             elif cell == 'O':
                 row.append('1')
+                hall += 1
             elif cell == 'o':
                 row.append('2')
+                ball += 1
             elif cell == 'P':
                 row.append('3')
+                position.append(len(stage))
+                position.append(len(row))
             elif cell == ' ':
                 row.append(cell)
         stage.append(row)
     return maps
 
 
-def makeArrToData (map):
-    for i in range(len(map)):    #i는 1스테이지
+
+
+
+
+makeMapToArr()
